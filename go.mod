03fa17fc71b9d9a451f46a4d63334e155a26c2c2
bodrovis <bodrovis@protonmail.com>
2024-05-02 17:16:27 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,8 @@
+module proj
+
+go 1.22.2
+
+require (
+	github.com/robotn/gohook v0.41.0 // indirect
+	github.com/vcaesar/keycode v0.10.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	hook "github.com/robotn/gohook"
+)
+
+func add() {
+	fmt.Println("--- Please press ctrl + shift + q to stop hook ---")
+	hook.Register(hook.KeyDown, []string{"q", "ctrl", "shift"}, func(e hook.Event) {
+		fmt.Println("ctrl-shift-q")
+		hook.End()
+	})
+
+	fmt.Println("--- Please press w---")
+	hook.Register(hook.KeyDown, []string{"w"}, func(e hook.Event) {
+		fmt.Println("w")
+	})
+
+	s := hook.Start()
+	<-hook.Process(s)
+}
+
+func main() {
+	add()
+}
